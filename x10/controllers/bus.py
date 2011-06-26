4b879514326fffaecd6a58097330c685ac16af01
Guillaume Libersat <glibersat@sigill.org>
2011-06-26 17:30:11 +0200

    correct logger syntax, add info message in bus

--- x10/controllers/bus.py
+++ x10/controllers/bus.py
@@ -1,6 +1,10 @@
+import logging
+
 import usb
 
 from .cm15 import CM15
+
+logger = logging.getLogger(__name__)
 
 class USBScanner(object):
     devices = (CM15,)
@@ -12,6 +16,7 @@ class USBScanner(object):
                 for X10Class in self.devices:
                     if dev.idVendor == X10Class.vendorId and \
                             dev.idProduct == X10Class.productId:
+                        logger.info("Found device %s" % X10Class)
                         return X10Class(dev)
 
         return None
