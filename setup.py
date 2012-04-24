612015d0de0631c5356b0435b44a4d6398dd06e4
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2012-04-24 10:50:39 +0200

    setup script updated, now using distutils

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-# Force use of distribute
-from distribute_setup import use_setuptools
-use_setuptools()
-
-# Import distribute tools
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name = 'Python X10',
@@ -13,12 +8,5 @@ setup(
     description = 'A python module to control X10 appliances',
     license = 'GNU GPL',
     
-    packages = ['x10'],
-    
-    #install_requires = ['pyusb'],
-    
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-    ],
+    packages = ['x10', 'x10.controllers', 'x10.devices'],
 )
