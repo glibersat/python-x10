336206bab66364013c112e2e67b0ba17706fe6d2
Guillaume Libersat <glibersat@sigill.org>
2013-08-12 11:47:08 +0200

    fix setup so the package can be submitted to Pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name = 'Python X10',
+    name = 'Python-X10',
     version = '0.1',
-    
+
     author = 'Guillaume Libersat',
     description = 'A python module to control X10 appliances',
+    url = 'https://github.com/glibersat/python-x10',
+
     license = 'GNU GPL',
     
     packages = ['x10', 'x10/controllers', 'x10/devices']
