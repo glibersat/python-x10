245014179c1cbe0804285af19a502c2d7473ad8a
Guillaume Libersat <glibersat@sigill.org>
2013-08-12 23:05:13 +0200

    add requirements to setup

--- setup.py
+++ setup.py
@@ -9,6 +9,11 @@ setup(
     url = 'https://github.com/glibersat/python-x10',
 
     license = 'GNU GPL',
+
+    install_requires=[
+        'pyusb>=1.0.0a1',
+        'pyserial>=2'
+    ],
     
     packages = ['x10', 'x10/controllers', 'x10/devices']
 )
